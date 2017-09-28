b46e0b76bb998c51df119daa276beec510020ad1
pcampese <p.campese@gmail.com>
2017-09-28 16:47:17 -0400

    changes

--- arguments.py
+++ arguments.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python
+"""Testing out python arguments."""
 
-print('Testing python arguments')
+import argparse
+
+def main():
+    """This is the main function."""
+
+    parser = argparse.ArgumentParser(description='My Sample Description.')
+    parser.add_argument('-f', '--filename',
+                        required=True)
+    parser.add_argument('-w', '--wizard_mode',
+                        choices=['Master', 'Apprentice'],
+                        default='Master')
+    parser.add_argument('integers',
+                        metavar='N',
+                        type=int,
+                        nargs='+',
+                        help='an integer for the accumulator')
+    parser.add_argument('--sum',
+                        dest='accumulate',
+                        action='store_const',
+                        const=sum,
+                        default=max,
+                        help='sum the integers (default: find the max)')
+
+    args = parser.parse_args()
+    print args.accumulate(args.integers)
+
+if __name__ == "__main__":
+    main()
